cdaf929e06a9e7238586daa7eeee6ab06716a0c3
Segus <segus3088@gmail.com>
2017-11-24 14:59:48 -0500

    iniciado metodos

--- GenerardorSQL.java
+++ GenerardorSQL.java
@@ -1,7 +1,26 @@
-public class GenerardorSQL extends Parent implements Interface {
+import java.util.Random;
+
+public class GenerardorSQL {
 
 	public static void main(String[] args) {
-		
+
+		//log("Hola");
+		int n;//cantidad datos a generar
+		n = Integer.parseInt(args[0]); // con esto tomo por consola la cantidade Datos a generar
+
+		GenerardorSQL objetoM = new GenerardorSQL();
+
+		objetoM.menu();
+
+	}
+
+	private void menu(){
+		//log("estoy en el menu");
+
+	}
+
+	private static void log(Object aMensajito){
+		System.out.println(String.valueOf(aMensajito));
 	}
 	
 }
